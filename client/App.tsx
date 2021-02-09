e85763724bd2bb9e8c2862fca526b48493636c25
hodesza <vinceho022@gmail.com>
2021-02-08 16:05:33 -0800

    Initialize front-end files Initialize React router with switch and routes Initialize component file structure
    Co-authored-by: Derek <DerekQuoc@gmail.com>

---
+++ client/App.tsx
@@ -0,0 +1,36 @@
+import React, {useContext ,useEffect} from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { render } from 'react-dom'; 
+// import Login 
+// import SignUp
+// import MainPage 
+// import CreateGame
+// import JoinGame
+// import Lobby
+// import Game
+// import Results
+
+const App: React.FC = React.memo (() => {
+return (
+    <Switch>
+      <Route path="/">
+
+      </Route>
+      <Route path="/signUp">
+      </Route>
+       <Route path="/mainPage">
+      </Route>
+       <Route path="/createGame">
+      </Route>
+       <Route path="/lobby">
+      </Route>
+       <Route path="/game">
+      </Route>
+       <Route path="/results">
+      </Route>
+    </Switch>
+  )
+});
+
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './client/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
